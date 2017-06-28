4b250c84a74a0f163f93225ed563a488b4057c0a
ralf <ralfoide@gmail.com>
2017-06-29 00:33:14 +0200

    RTAC: Basic automation fragment.

--- android/RTAC/app/src/main/java/com/alflabs/rtac/activity/MainActivity.java
+++ android/RTAC/app/src/main/java/com/alflabs/rtac/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.alflabs.rtac.activity;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.alflabs.rtac.R;
 import com.alflabs.rtac.app.MainApp;
