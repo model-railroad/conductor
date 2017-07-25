9f0742a133c7b4bca5fc5edabd0e17836d8e53ea
ralf <ralfoide@gmail.com>
2017-07-24 22:16:58 -0400

    RTAC: Hack to make it compatible with Android Studio 2.x and Jack for Java 1.8 support

--- android/RTAC/app/src/main/java/com/alflabs/rtac/app/IAppComponent.java
+++ android/RTAC/app/src/main/java/com/alflabs/rtac/app/IAppComponent.java
@@ -1,5 +1,6 @@
 package com.alflabs.rtac.app;
 
+import com.alflabs.rtac.activity.ActivityContextModule;
 import com.alflabs.rtac.activity.IMainActivityComponent;
 import dagger.Component;
 
@@ -16,5 +17,5 @@ public interface IAppComponent
     // Note: this line should not be needed (create() is inherited from the Component.Factory))
     // but there's a bug in Jack/ECJ, tracked at https://code.google.com/p/android/issues/detail?id=223549
     // This is only needed when the new (now already obsoleted) Jack compiler is used for Java 8 support.
-    // -- IMainActivityComponent create(ActivityContextModule activityContextModule);
+    IMainActivityComponent create(ActivityContextModule activityContextModule);
 }
