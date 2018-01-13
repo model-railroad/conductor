4e82e272443040b75b170de2b3e80134de85bd5d
ralf <ralfoide@gmail.com>
2018-01-12 23:08:47 -0800

    Cleanup.

--- android/RTAC/LibManifest/src/main/java/com/alflabs/manifest/MapInfo.java
+++ android/RTAC/LibManifest/src/main/java/com/alflabs/manifest/MapInfo.java
@@ -18,11 +18,6 @@
 
 package com.alflabs.manifest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
-
 /**
  * Information on one Map served by Conductor.
  * <p/>
