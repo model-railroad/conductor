f4bd0b504346bf88ebf978015da574a2f7f87a9c
ralf <ralfoide@gmail.com>
2022-04-04 22:19:04 -0700

    Experimental: Kotlin DSL, try running using Java Script from JSR223.

---
+++ jmri/conductor/engine2k/src/main/kotlin/com/alfray/conductor/v2/host/JavaScriptEval.kt
@@ -0,0 +1,53 @@
+package com.alfray.conductor.v2.host
+
+import com.alfray.conductor.v2.script.IConductor
+import java.util.*
+import javax.script.Bindings
+import javax.script.ScriptContext
+import javax.script.ScriptEngineManager
+import kotlin.script.experimental.api.SourceCode
+
+// References:
+// - https://github.com/JetBrains/kotlin/blob/master/libraries/scripting/jvm-host/src/kotlin/script/experimental/jvmhost/jsr223/KotlinJsr223ScriptEngineImpl.kt
+// - https://github.com/JetBrains/kotlin/blob/master/compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/repl/KotlinJsr223JvmScriptEngineBase.kt
+// - https://github.com/JetBrains/kotlin/blob/master/compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/repl/ReplState.kt
+
+class JavaScriptEval {
+    fun eval(source: SourceCode) : IConductor {
+        val manager = ScriptEngineManager()
+        val engine = manager.getEngineByExtension("kts")
+
+        val ret = engine.eval(source.text)
+
+        println("Eval: $ret")
+        println("Engine Binding: " + engine.getBindings(ScriptContext.ENGINE_SCOPE))
+        println("Global Binding: " + engine.getBindings(ScriptContext.GLOBAL_SCOPE))
+        println("Context: " + engine.context)
+        println("Context Engine Binding: " + engine.context.getBindings(ScriptContext.ENGINE_SCOPE))
+        println("Context Global Binding: " + engine.context.getBindings(ScriptContext.GLOBAL_SCOPE))
+
+        dumpVars("engine", engine.getBindings(ScriptContext.ENGINE_SCOPE))
+        dumpVars("global", engine.getBindings(ScriptContext.GLOBAL_SCOPE))
+
+        return ret as IConductor
+    }
+
+    private fun dumpVars(prefix: String, bindings: Bindings?) {
+        for (entry in bindings!!.entries) {
+            println(prefix + " [" + entry.key + "] => " + entry.value);
+        }
+
+        // Variables can be seen in:
+        // bindings["kotlin.script.state"] as AggregatedReplStageState
+        //  .state2 as JvmReplEvaluatorState
+        //      .history as ReplStageHistoryWithReplace
+        //          [0].item.second as ScriptingHost
+        //              field B320...
+        //              field res0 = CScriptBuilder (return value from script)
+
+//        val state = bindings["kotlin.script.state"]
+//        if (state is AggregatedReplStageState) {
+//
+//        }
+    }
+}
---
+++ jmri/conductor/engine2k/src/test/kotlin/com/alfray/conductor/v2/script/JavaScriptTest2k.kt
@@ -0,0 +1,64 @@
+package com.alfray.conductor.v2.script
+
+import com.alfray.conductor.v2.host.ConductorScriptHost
+import com.alfray.conductor.v2.host.JavaScriptEval
+import com.google.common.io.Resources
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import java.lang.Exception
+import kotlin.script.experimental.api.EvaluationResult
+import kotlin.script.experimental.api.ResultWithDiagnostics
+import kotlin.script.experimental.api.ScriptDiagnostic
+import kotlin.script.experimental.api.SourceCode
+import kotlin.script.experimental.host.StringScriptSource
+import kotlin.script.experimental.host.UrlScriptSource
+
+//@Suppress("UnstableApiUsage")
+class JavaScriptTest2k {
+
+    private val scriptEval = JavaScriptEval()
+    private lateinit var conductorImpl: ConductorImpl
+
+    @Before
+    fun setUp() {
+    }
+
+    private fun loadScriptFromFile(scriptName: String) : IConductor {
+        val scriptPath = "v2/script/$scriptName.conductor.kts"
+        val scriptUrl = Resources.getResource(scriptPath)!!
+        val source = UrlScriptSource(scriptUrl)
+        return loadScript(source)
+    }
+
+    private fun loadScriptFromText(scriptName: String = "local", scriptText: String) : IConductor {
+        val source = StringScriptSource(scriptText, scriptName)
+        return loadScript(source)
+    }
+
+    private fun loadScript(source: SourceCode) : IConductor {
+        val impl = scriptEval.eval(source)
+        conductorImpl = impl as ConductorImpl
+        return impl
+    }
+
+    @Test
+    fun testLoadScriptAndEval() {
+        loadScriptFromFile("sample_v2")
+        assertThat(conductorImpl).isNotNull()
+    }
+
+    @Test
+    fun testEvalDirect() {
+        loadScriptFromText(scriptText = """
+            import com.alfray.conductor.v2.script.ConductorImpl
+            ConductorImpl().apply {
+                val s1 = sensor("S01")
+                val s2 = sensor("S01")
+            }
+        """.trimIndent())
+        assertThat(conductorImpl).isNotNull()
+        assertThat(conductorImpl.sensors.keys).contains("S01")
+    }
+}
