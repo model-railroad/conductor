6ca4fd5d83b3e3ab853c6f277d17aa7ce5634588
ralf <ralfoide@gmail.com>
2022-06-16 22:11:43 -0700

    Experimental: Engine2k, port JMRI Throttle logic.

--- jmri/conductor/engine2k/src/main/kotlin/com/alfray/conductor/v2/script/impl/Throttle.kt
+++ jmri/conductor/engine2k/src/main/kotlin/com/alfray/conductor/v2/script/impl/Throttle.kt
@@ -20,6 +20,11 @@ package com.alfray.conductor.v2.script.impl
 
 import com.alflabs.conductor.jmri.IJmriProvider
 import com.alflabs.conductor.jmri.IJmriThrottle
+import com.alflabs.conductor.util.EventLogger
+import com.alflabs.kv.IKeyValue
+import com.alflabs.manifest.Prefix
+import com.alflabs.utils.IClock
+import com.alflabs.utils.ILogger
 import com.alfray.conductor.v2.dagger.Script2kScope
 import com.alfray.conductor.v2.script.dsl.DccSpeed
 import com.alfray.conductor.v2.script.dsl.Delay
@@ -36,6 +41,10 @@ internal interface IThrottleFactory {
 }
 
 internal class Throttle @AssistedInject constructor(
+    private val clock: IClock,
+    private val logger: ILogger,
+    private val keyValue: IKeyValue,
+    private val eventLogger: EventLogger,
     private val jmriProvider: IJmriProvider,
     @Assisted override val dccAddress: Int
 ) : VarName(), IThrottle, IExecEngine {
@@ -44,7 +53,14 @@ internal class Throttle @AssistedInject constructor(
     private var _sound = false
     private var _repeat = Delay(0)
     private var _f = FBits()
+    /** Delay in seconds after the last command sent to JMRI before repeating the current speed. */
+    private var repeatSpeedSeconds = 0
+    private var lastJmriTS: Long = 0L
     private lateinit var jmriThrottle: IJmriThrottle
+
+    private companion object {
+        val TAG: String = Throttle::class.java.simpleName
+    }
 
     override val speed: DccSpeed
         get() = _speed
@@ -92,9 +108,101 @@ internal class Throttle @AssistedInject constructor(
 
     override fun onExecStart() {
         jmriThrottle = checkNotNull(jmriProvider.getThrottle(dccAddress))
+        updateKV(dccAddress, _speed.speed)
     }
 
     override fun onExecHandle() {
         // no-op
+    }
+
+    /**
+     * Repeats the current speed if the specified delay as expired between now and the
+     * last command sent to JMRI for this throttle.
+     *
+     *
+     * The call does nothing if [.getRepeatSpeedSeconds] <= 0.
+     */
+    fun repeatSpeed() {
+        if (repeatSpeedSeconds < 1) {
+            return
+        }
+        val elapsedMs: Long = clock.elapsedRealtime() - lastJmriTS
+        if (elapsedMs >= 1000 * repeatSpeedSeconds) {
+            setSpeed(_speed)
+        }
+    }
+
+    /**
+     * Sets the throttle speed and direction.
+     * Speed 0 means stopped, a positive number for forward and a negative number for reverse.
+     */
+    fun setSpeed(speed: DccSpeed) {
+        val speedChange = speed != _speed
+        _speed = speed
+
+        val dccAddress = jmriThrottle.dccAddress
+        try {
+            if (speedChange) {
+                eventLogger.logAsync(
+                    EventLogger.Type.DccThrottle,
+                    dccAddress.toString(),
+                    speed.speed.toString()
+                )
+            }
+            jmriThrottle.setSpeed(speed.speed)
+        } catch (e: Throwable) {
+            logger.d(
+                TAG,
+                "[$dccAddress] setSpeed exception: $e"
+            )
+        }
+        try {
+            updateKV(dccAddress, speed.speed)
+        } catch (e: Throwable) {
+            logger.d(
+                TAG,
+                "[$dccAddress] getDccAddress exception: $e"
+            )
+        }
+
+        lastJmriTS = clock.elapsedRealtime()
+// TODO(implement throttle speed listener)
+//        if (speedListener != null && speedChange) {
+//            try {
+//                speedListener.accept(speed.speed)
+//            } catch (e: Throwable) {
+//                logger.d(
+//                    TAG,
+//                    "[$dccAddress] mSpeedListener exception: $e"
+//                )
+//            }
+//        }
+    }
+
+// TODO(implement throttle speed listener)
+//    fun setSpeedListener(speedListener: IIntFunction) {
+//        this.speedListener = speedListener
+//    }
+
+    fun eStop() {
+        // Do a "soft" stop to speed 0. This also sets this object's state properly.
+        setSpeed(DccSpeed(0))
+
+        // Ask JMRI to send an e-stop command to all throttles
+        try {
+            jmriThrottle.eStop()
+        } catch (e: Throwable) {
+            logger.d(
+                TAG,
+                "[$dccAddress] eStop exception: $e"
+            )
+        }
+    }
+
+    private fun updateKV(address: Int, speed: Int) {
+        keyValue.putValue(
+            "${Prefix.DccThrottle}$address",
+            speed.toString(), true /*broadcast*/
+        )
     }
 }
