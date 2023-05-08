03c47c5f751a3d334effe5652e4d9daed916a542
Ralf <ralfoide@gmail.com>
2023-05-08 08:21:42 -0700

    Dev: Install script with --skip-tests.

--- jmri/conductor/_install.sh
+++ jmri/conductor/_install.sh
@@ -24,9 +24,15 @@ if [[ ! -d "$JDIR" ]]; then
   echo "Invalid script. Please adjust $0"
 fi
 
+GRADLE_CMD="build"
+if [[ "$1" == "--skip-tests" ]]; then
+    GRADLE_CMD="fatJar"
+    shift
+fi
+
 set -e
 echo; echo "---- Building with gradle..."
-./gradlew build --console=plain $@
+./gradlew $GRADLE_CMD --console=plain $@
 echo; echo "---- Copying files..."
 op src/Conductor.py                          jython/Conductor.py
 op build/libs/conductor-2.0-SNAPSHOT-all.jar lib/conductor.jar
