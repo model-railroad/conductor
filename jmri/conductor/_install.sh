fa57c11db1dfb6b8b9a407c8af3917262faf0960
ralf <ralfoide@gmail.com>
2024-04-27 19:12:40 -0700

    Conductor: Enforce Java 1.8 in wrapper build script.

--- jmri/conductor/_install.sh
+++ jmri/conductor/_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 JDIR=""
-if [[ $(uname) =~ CYGWIN_.* ]]; then
+if [[ $(uname) =~ CYGWIN_.* || $(uname) =~ MSYS_.* ]]; then
   JDIR=/cygdrive/c/Program\ Files\ \(x86\)/JMRI
 
   function op() {
@@ -23,6 +23,21 @@ fi
 if [[ ! -d "$JDIR" ]]; then
   echo "Invalid script. Please adjust $0"
 fi
+
+# We currently need Java 1.8
+if ! grep -qs "1.8" $(java -version 2>&1) ; then
+  if [[ $(uname) =~ CYGWIN_.* || $(uname) =~ MSYS_.* ]]; then
+    PF=$(cygpath "$PROGRAMFILES")
+    JS=$(find "$PF/Java" -type f -name javac.exe | grep 1.8 | sort -r | head -n 1)
+    JS=$(cygpath -w "${JS//\/bin*/}")
+  else
+    echo "TBD"
+    exit 1
+  fi
+  export JAVA_HOME="$JS"
+  echo "## New JAVA_HOME = $JAVA_HOME"
+fi
+
 
 GRADLE_CMD="build"
 if [[ "$1" == "--skip-tests" ]]; then
