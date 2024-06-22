2e2829741f2610ee23dd37745cad0cf61b279816
ralf <ralfoide@gmail.com>
2024-06-22 15:43:33 +0200

    Exp: Script 55, v11, experiment with simpler recovery for BL's virtual block.

--- jmri/conductor/engine2k/src/test/kotlin/com/alfray/conductor/v2/script/ValidateScriptsSyntax2k.kt
+++ jmri/conductor/engine2k/src/test/kotlin/com/alfray/conductor/v2/script/ValidateScriptsSyntax2k.kt
@@ -176,4 +176,13 @@ class ValidateScriptsSyntax2k : ScriptTest2kBase() {
         loadScriptFromFile("script_55_v10_bl204+ml8330+1067+tl6119_sat")
         assertResultNoError()
     }
+
+    @Test
+    fun testScript55_v11() {
+        fileOps.writeBytes(
+            "<svg/>".toByteArray(Charsets.UTF_8),
+            fileOps.toFile("v2", "script", "src", "test", "resources", "v2", "Conductor Map Mainline 1.svg"))
+        loadScriptFromFile("script_55_v11_bl204+ml8330+1067+tl6119_sat")
+        assertResultNoError()
+    }
 }
