f5cb6f4008f8832edce831494dd4653a7dda8d9e
ralf <ralfoide@gmail.com>
2024-12-18 15:36:49 -0800

    Conductor: Script 30 v1, switch Mainline from 8330 to 8312.

--- jmri/conductor/engine2k/src/test/kotlin/com/alfray/conductor/v2/script/ValidateScriptsSyntax2k.kt
+++ jmri/conductor/engine2k/src/test/kotlin/com/alfray/conductor/v2/script/ValidateScriptsSyntax2k.kt
@@ -81,4 +81,10 @@ class ValidateScriptsSyntax2k : ScriptTest2kBase() {
         loadScriptFromFile("script_v29.04_bl204+ml8330+1225+tl6885.conductor.kts")
         assertResultNoError()
     }
+
+    @Test
+    fun testScript30() {
+        loadScriptFromFile("script_v30.01_bl204+ml8312+1225+tl6885.conductor.kts")
+        assertResultNoError()
+    }
 }
--- jmri/conductor/src/main/java/com/alflabs/conductor/v2/DevEntryPoint2.java
+++ jmri/conductor/src/main/java/com/alflabs/conductor/v2/DevEntryPoint2.java
@@ -26,7 +26,7 @@ public class DevEntryPoint2 {
         System.out.println(TAG + " - Start");
         EntryPoint2 entry = new EntryPoint2();
 
-        entry.init("script_v29.04_bl204+ml8330+1225+tl6885.conductor.kts");
+        entry.init("script_v30.01_bl204+ml8312+1225+tl6885.conductor.kts");
         entry.runDevLoop();
         System.out.println(TAG + " - End");
     }
