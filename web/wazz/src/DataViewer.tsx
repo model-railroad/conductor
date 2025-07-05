954e447da027bb0cd293a37d8158ef9941738f7e
ralf <ralfoide@gmail.com>
2025-07-05 15:22:17 -0700

    Wazz: DataViewer, display system table.

--- web/wazz/src/DataViewer.tsx
+++ web/wazz/src/DataViewer.tsx
@@ -1,5 +1,6 @@
 import {type ReactElement, useEffect, useState} from "react";
-import {Table} from "react-bootstrap";
+import {Button, Table} from "react-bootstrap";
+import {DateTime} from "luxon";
 
 const RTAC_JSON_URL = "https://www.alfray.com/cgi/rtac_status.py"
 const FAKE_JSON_URL = "fake_data.json"
@@ -8,13 +9,12 @@ const JSON_URL = FAKE_JSON_URL
 
 // -- Interface from the JSON payload
 
-interface TimestampValue {
-    "ts": string;       // ISO date
-    "value": string;
-}
-
 interface Timestamp {
     "ts": string;       // ISO date
+}
+
+interface TimestampValue extends Timestamp{
+    "value": string;
 }
 
 interface DepartDict {
@@ -30,25 +30,21 @@ interface RouteStatsDict {
 }
 
 interface RtacJsonData {
-    "conductor"?: TimestampValue; // on or off values
-    "computer"?: TimestampValue;
-    "computer-vision"?: TimestampValue;
-    depart?: DepartDict;
-    toggle?: ToggleDict;
-    route_stats?: RouteStatsDict;
+    [key: string]: TimestampValue|DepartDict|ToggleDict|RouteStatsDict;
 }
 
 // -- Interface for display in Wazz
 
 interface WazzStatusEntry {
+    indent: boolean;
     state?: boolean;    // (on, off) state, or just a timestamp
     label: string;
     sublabel?: string;
-    ts: Date;
+    ts: DateTime;
 }
 
 interface WazzRouteEntry {
-    ts: Date;
+    ts: DateTime;
     name: string;
     runs: number;
     error: boolean;
@@ -102,6 +98,48 @@ function DataViewer(): ReactElement {
             status: [],
         }
 
+        function _appendTsValue(key1: string, key2?: string) {
+            // 2021-08-27 adjust computer names: "computer" alone is legacy for "computer-consist".
+            let label = key1;
+            if (label === "computer") {
+                label = "computer-consist";
+            }
+            label = label.replaceAll("-" , " ");
+            const indent = (label === "depart");
+
+            let data = rtac[key1] as never;
+            if (key2 !== undefined) {
+                data = data[key2];
+            }
+            const ts = data as Timestamp;
+            const dt = DateTime.fromISO(ts.ts);
+
+            const state: boolean|undefined = ("value" in data)
+                ? String(data["value"]).toLowerCase() === "on"
+                : undefined;
+
+            const entry: WazzStatusEntry = {
+                indent: indent,
+                state: state,
+                label: label,
+                sublabel: key2,
+                ts: dt,
+            };
+
+            result.status.push(entry);
+        }
+
+        _appendTsValue("computer");
+        _appendTsValue("computer-vision");
+        _appendTsValue("conductor");
+
+        _appendTsValue("toggle", "passenger");
+        _appendTsValue("depart", "passenger");
+        _appendTsValue("depart", "freight");
+        _appendTsValue("toggle", "branchline");
+        _appendTsValue("depart", "branchline");
+        _appendTsValue("depart", "trolley");
+
         return result;
     }
 
@@ -109,26 +147,52 @@ function DataViewer(): ReactElement {
         return <div className="pcs-status"> {status} </div>;
     }
 
+    function formatDate(dateTime: DateTime) {
+        const pacificDt = dateTime.setZone("America/Los_Angeles"); // PST or PDT
+        const dateString2 = pacificDt.toLocaleString(DateTime.DATETIME_FULL);
+        const relativeToNow = pacificDt.toRelative();
+
+        return (
+            <span title={dateTime.toISO( {
+                format: "extended",
+                suppressMilliseconds: true
+            })}>
+                {dateString2} ({relativeToNow})
+            </span>
+        )
+    }
+
+    function formatButtonOnOff(state?: boolean) {
+        if (state === undefined) {
+            return <></>;
+        }
+        if (state) {
+            return <Button className="wazz-btn" variant="success" size="sm">ON</Button>
+        } else {
+            return <Button className="wazz-btn" variant="danger" size="sm">OFF</Button>
+        }
+    }
+
     function generateSystemStatus() {
         if (loading) {
-            return <span className="pcs-loading">...</span>;
+            return <span className="wazz-loading">...</span>;
         }
 
         return (
-            <Table striped bordered hover>
+            <Table striped bordered hover className="wazz-table wazz-system-table">
                 <thead>
                 <tr>
-                    <th>System Status</th>
+                    <th colSpan={3}>System Status</th>
                     <th>Last Updated</th>
                 </tr>
                 </thead>
                 <tbody>
                 { wazzData.status.map((entry, index) => (
                     <tr key={index}>
-                        <td> { entry.label } </td>
-                        <td> { entry.sublabel } </td>
-                        <td> { entry.state } </td>
-                        <td> { entry.ts.toLocaleString() } </td>
+                        <td className={`wazz-status-text wazz-indent-${entry.indent}`}> { entry.label } </td>
+                        <td className="wazz-status-text"> { entry.sublabel } </td>
+                        <td> { formatButtonOnOff(entry.state) } </td>
+                        <td> { formatDate(entry.ts) } </td>
                     </tr>
                 )) }
                 </tbody>
@@ -138,11 +202,11 @@ function DataViewer(): ReactElement {
 
     function generateRouteStatus() {
         if (loading) {
-            return <span className="pcs-loading">...</span>;
+            return <span className="wazz-loading">...</span>;
         }
 
         return (
-            <Table striped bordered hover>
+            <Table striped bordered hover className="wazz-table wazz-routes-table">
                 <thead>
                 <tr>
                     <th>Finished At</th>
