d883c82c8e02a3399fe0a82ae4e77f6d0fe42a5d
ralf <ralfoide@gmail.com>
2025-07-06 07:36:16 -0700

    Wazz: Fix export setting.

--- web/wazz/src/GitBuild.tsx
+++ web/wazz/src/GitBuild.tsx
@@ -1,4 +1,4 @@
-export const GIT_HASH_STR = "web @ 66242e65";
-export const GIT_LONG_STR = "Build web @ 66242e65, 2025-07-06 02:11:31 -0700";
+export const GIT_HASH_STR = "main @ 4ba5cdc9";
+export const GIT_LONG_STR = "Build main @ 4ba5cdc9, 2025-07-06 07:09:20 -0700";
 export const VERSION_MAJOR = "2";
 export const VERSION_MINOR = "0";
